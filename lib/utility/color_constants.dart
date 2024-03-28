78e9269070be32e0d167c9dfef978b06b51d7fab
Arnoldaditya17 <adityasinghrajput@sistec.ac.in>
2024-03-29 00:39:14 +0530

    Initial commit

---
+++ lib/utility/color_constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color black = Colors.black;
+  static Color white = Colors.white;
+  static Color yellow = const Color(0xFFfdbf00);
+  static Color yellowDark = const Color(0xFFe48400);
+  static Color orange = const Color(0xFFfe8101);
+  static Color orangeDark = const Color(0xFFa55600);
+  static Color grey = const Color(0xFF9E9E9E);
+  static Color darkGrey = const Color(0xFF9E9E9E);
+  static Color lightGrey = const Color(0xFFFAFAFA);
+  static Color red = const Color(0xFFD32F2F);
+  static Color green = const Color(0xFF388E3C);
+}
+
+class SPref {
+  static const String isLight = "isLight";
+}
